808194d37822e266362a375a907c676e44a8a942
DESKTOP-QUL61PF\Uriel David <uriel.azevedo.ext@spms.min-saude.pt>
2023-04-02 18:25:51 +0100

    test animation in page details and builds

--- listApp/src/pages/Details.tsx
+++ listApp/src/pages/Details.tsx
@@ -1,20 +1,28 @@
-import { IonBackButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonViewWillEnter } from '@ionic/react';
-import { useState } from 'react';
+import { createAnimation, IonBackButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonViewWillEnter } from '@ionic/react';
+import { useRef, useState } from 'react';
 import { useData } from '../hooks/useData';
 import { UserDetailPageProps } from '../interfaces/UserDetailsPage';
 
 const Details: React.FC<UserDetailPageProps> = ({ match }) => {
   const { getUserByEmail } = useData();
   const [user, setUser] = useState<any>(null);
+  const [animation, setAnimation] = useState(null);
+  const ionCardRef = useRef(null as any);
 
   const getUser = async () => {
     const user = await getUserByEmail(match.params.email);
-    console.log(user);
     setUser(user);
   }
 
   useIonViewWillEnter(async () => {
     getUser();
+    createAnimation('myId')
+      .addElement(ionCardRef.current)
+      .duration(2000)
+      .fromTo('opacity', 100, 0)
+      .iterations(4)
+      .direction('alternate')
+      .play();
   });
 
   return (
@@ -28,7 +36,7 @@ const Details: React.FC<UserDetailPageProps> = ({ match }) => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonCard>
+        <IonCard ref={ionCardRef}>
           <IonCardHeader>
             <IonCardTitle>
               { user?.name.first } { user?.name.last }
